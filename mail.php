30e71a3ab7c60630e64875d3f2d7b678558ec576
F4A28LoeyLou <72017799+F4A28LoeyLou@users.noreply.github.com>
2020-12-13 21:30:09 +0800

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$country = $_POST['country'];
+$queries= $_POST['queries'];
+
+
+echo "Thank You" 
+?>
+
+
+
+</body>
+</html>
